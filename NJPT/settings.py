3ec0aaf70212c2872199cafa1696f558ebf87513
Filippo Bellei <filippobellei@gmail.com>
2023-06-16 12:28:11 +0200

    Add homepage

--- NJPT/settings.py
+++ NJPT/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'NJPT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
