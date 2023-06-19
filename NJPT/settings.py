87a34f4264eb88b94599f083286abf3081befe7b
Filippo Bellei <filippobellei@gmail.com>
2023-06-19 13:11:41 +0200

    Add PostgreSQL support

--- NJPT/settings.py
+++ NJPT/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+# from dotenv import load_dotenv
+
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'NJPT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'uomoazione',
+        'PASSWORD': 'Tjb2Sr4HaVie',
+        'HOST': 'ep-odd-wave-783435.eu-central-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
