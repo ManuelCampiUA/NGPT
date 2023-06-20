ce7097735b8cd021db3ec0608ca0ef94489baea8
Filippo Bellei <filippobellei@gmail.com>
2023-06-20 09:45:50 +0200

    Add DB access credentials to .env

--- NJPT/settings.py
+++ NJPT/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'NJPT.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'neondb',
-        'USER': 'uomoazione',
-        'PASSWORD': 'Tjb2Sr4HaVie',
-        'HOST': 'ep-odd-wave-783435.eu-central-1.aws.neon.tech',
-        'PORT': '5432',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
