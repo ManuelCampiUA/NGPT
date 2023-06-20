78342918f819e6b666f05f4e2a1fa1dacdc32dbf
Filippo Bellei <filippobellei@gmail.com>
2023-06-20 09:54:29 +0200

    Add Django Secret Key to .env

--- NJPT/settings.py
+++ NJPT/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yqe5dr2j7361_*nxtbpybq!q_al1%prb(!t)axf776h@#l=pst'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
