404771e13a6ba6a8cb529ecc6f3c03bc06e2a9ed
Filippo Bellei <filippobellei@gmail.com>
2023-07-04 13:34:10 +0200

    Remove textarea.js

--- app/static/js/textarea.js
+++
@@ -1,6 +0,0 @@
-const textarea = document.getElementById('myTextarea');
-
-textarea.addEventListener('input', function () {
-    this.style.height = 'auto'; // Ripristina l'altezza predefinita
-    this.style.height = this.scrollHeight + 'px'; // Imposta l'altezza in base al contenuto
-});
