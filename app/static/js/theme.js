266565bc31ad0a81bd8be63801eac673854b9ac3
Manuel Campi <manuelcampi20@gmail.com>
2023-07-11 12:24:25 +0200

    modificate icone in formato svg, cambiata posizione toggle e messo pdf icon a li

--- app/static/js/theme.js
+++ app/static/js/theme.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     themeIcon.onclick = () => {
         document.body.classList.toggle('dark_theme');
         if (document.body.classList.contains('dark_theme')) {
-            themeIcon.src = 'static/img/moon.png';
+            themeIcon.src = 'static/img/moon.svg';
         } else {
-            themeIcon.src = 'static/img/sun.png';
+            themeIcon.src = 'static/img/sun.svg';
         }
     }
 });
