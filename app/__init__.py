328c413c239656ee52671770d8c5b413dd39ab09
Filippo Bellei <filippobellei@gmail.com>
2023-07-21 12:50:22 +0200

    Improve cookie security

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,12 @@ def internal_server_error(e):
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+    app.config.update(
+        SECRET_KEY=environ.get("SECRET_KEY"),
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SAMESITE="Lax",
+    )
     app.register_error_handler(500, internal_server_error)
     app.register_blueprint(database)
     app.register_blueprint(auth)
