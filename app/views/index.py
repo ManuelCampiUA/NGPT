0ae0c6d3b1d709986b1575b3948e63e79918ebf6
ManuelCampi <manuelcampi20@gmail.com>
2023-08-03 14:17:21 +0200

    stylizing 403 error page

--- app/views/index.py
+++ app/views/index.py
@@ -6,3 +6,8 @@ index_views = Blueprint("index_views", __name__)
 @index_views.route("/")
 def home():
     return render_template("index.html")
+
+# Da refactorizzare
+@index_views.route("/error")
+def error():
+    return render_template("error.html")
