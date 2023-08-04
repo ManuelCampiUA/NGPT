8f448137610281afbb6076e7436cc1e6c2271784
Filippo Bellei <filippobellei@gmail.com>
2023-08-04 12:11:44 +0200

    Refactor CSS

--- app/static/js/theme.js
+++ app/static/js/theme.js
@@ -1,15 +1,15 @@
-const themeIcon = document.getElementById('toggle_dark');
+const themeIcon = document.getElementById('toggle-dark');
 
 function setDarkMode() {
-    document.body.classList.remove('light_theme');
-    document.body.classList.add('dark_theme');
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
     localStorage.setItem('theme', 'dark');
     themeIcon.src = 'static/img/sun.svg';
 }
 
 function setLightMode() {
-    document.body.classList.remove('dark_theme');
-    document.body.classList.add('light_theme');
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
     localStorage.setItem('theme', 'light');
     themeIcon.src = 'static/img/moon.svg';
 }
@@ -20,7 +20,7 @@ if (localStorage.getItem('theme') === 'dark')
 document.addEventListener('DOMContentLoaded', () => {
     themeIcon.addEventListener('click', () => {
         const currentTheme = document.body.classList[0];
-        if (currentTheme === 'light_theme') {
+        if (currentTheme === 'light-theme') {
             setDarkMode();
             return;
         }
