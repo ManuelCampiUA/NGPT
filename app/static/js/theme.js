94ba20a316215c30fb7871766841d6a698efbce8
ManuelCampi <manuelcampi20@gmail.com>
2023-08-08 17:37:03 +0200

    darkmode per ñpgp base e index

--- app/static/js/theme.js
+++ app/static/js/theme.js
@@ -1,10 +1,14 @@
 const themeIcon = document.getElementById('toggle-dark');
+const themeLogo = document.getElementById('NapolitanoGPT-logo');
+const themeLogoIndex = document.getElementById('NapolitanoGPT-logo-Index');
 
 function setDarkMode() {
     document.body.classList.remove('light-theme');
     document.body.classList.add('dark-theme');
     localStorage.setItem('theme', 'dark');
     themeIcon.src = 'static/img/sun.svg';
+    themeLogo.src = 'static/img/logodark.png';
+    themeLogoIndex.src='static/img/logodark.png';
 }
 
 function setLightMode() {
@@ -12,6 +16,8 @@ function setLightMode() {
     document.body.classList.add('light-theme');
     localStorage.setItem('theme', 'light');
     themeIcon.src = 'static/img/moon.svg';
+    themeLogo.src = 'static/img/logo.png';
+    themeLogoIndex.src='static/img/logo.png';
 }
 
 if (localStorage.getItem('theme') === 'dark')
