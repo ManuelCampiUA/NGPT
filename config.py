bb20e6dee399ed3170a18c339e40df09d8fece95
Filippo Bellei <filippobellei@gmail.com>
2023-08-25 19:19:25 +0200

    Fix config.py bug

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from json import load
 
-SECRET_KEY = secret_key = load(open("config.json"))["secretKey"]
+SECRET_KEY = secret_key = load(open("env.json"))["secretKey"]
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = "Lax"
